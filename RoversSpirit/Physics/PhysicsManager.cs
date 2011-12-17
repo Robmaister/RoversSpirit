e6eabcb3febe24abba39c8e7a1b0648636ce1bce
Robert Rouhani <robert.rouhani@gmail.com>
2011-12-17 12:55:20 -0800

    Fixed physics

--- RoversSpirit/Physics/PhysicsManager.cs
+++ RoversSpirit/Physics/PhysicsManager.cs
@@ -4,16 +4,6 @@ using OpenTK;
 
 namespace RoversSpirit.Physics
 {
-	[Flags]
-	public enum Axes
-	{
-		None = 0,
-		Left = 1,
-		Right = 2,
-		Top = 4,
-		Bottom = 8
-	}
-
 	public static class PhysicsManager
 	{
 		public static bool IsColliding(Entity ent1, Entity ent2)
@@ -40,7 +30,6 @@ namespace RoversSpirit.Physics
 		public static Vector2 ReactCollision(Entity ent1, Entity ent2)
 		{
 			Vector2 sizes = ent1.Size + ent2.Size;
-
 			Vector2 colDist = ent1.Position - ent2.Position;
 
 			//abs
@@ -49,61 +38,20 @@ namespace RoversSpirit.Physics
 
 			colDist += (sizes / 2);
 
+			//(combined sizes) - (combined world sizes from one corner to the other) = minimum distance to exit
 			Vector2 minMoveDist = sizes - colDist;
 
-			AABB bb1 = ent1.BoundingBox, bb2 = ent2.BoundingBox;
-
+			//set the proper direction for movement
 			if (ent1.Position.X <= ent2.Position.X) minMoveDist.X = -minMoveDist.X;
 			if (ent1.Position.Y <= ent2.Position.Y) minMoveDist.Y = -minMoveDist.Y;
 
-			Axes collidingAxes = GetCollidingAxes(bb1, bb2);
-
-			if ((collidingAxes & Axes.Left) != Axes.Left && (collidingAxes & Axes.Right) != Axes.Right)
+			//use only the smaller value (vertex-vertex collisions snap to one axis or the other, left/right axes if equal)
+			if (Math.Abs(minMoveDist.X) > Math.Abs(minMoveDist.Y))
 				minMoveDist.X = 0;
-
-			if ((collidingAxes & Axes.Top) != Axes.Top && (collidingAxes & Axes.Bottom) != Axes.Bottom)
+			else
 				minMoveDist.Y = 0;
 
-			if (collidingAxes.Count() == 2)
-			{
-				if (Math.Abs(minMoveDist.X) > Math.Abs(minMoveDist.Y))
-					minMoveDist.X = 0;
-				else
-					minMoveDist.Y = 0;
-			}
-
 			return minMoveDist;
-		}
-
-		public static Axes GetCollidingAxes(AABB box1, AABB box2)
-		{
-			Axes a = Axes.None;
-
-			if (box1.Top <= box2.Top && box1.Top > box2.Bottom) a |= Axes.Bottom;
-			if (box1.Bottom >= box2.Bottom && box1.Bottom < box2.Top) a |= Axes.Top;
-			if (box1.Right >= box2.Right && box1.Right > box2.Left) a |= Axes.Right;
-			if (box1.Left <= box2.Left && box1.Left < box2.Right) a |= Axes.Left;
-
-			if ((a & (Axes.Top | Axes.Bottom)) == (Axes.Top | Axes.Bottom))
-				a &= ~(Axes.Top | Axes.Bottom);
-			if ((a & (Axes.Left | Axes.Right)) == (Axes.Left | Axes.Right))
-				a &= ~(Axes.Left | Axes.Right);
-
-			return a;
-		}
-
-		/// <summary>
-		/// Counts the number of flags set. Thanks stackoverflow!
-		/// </summary>
-		/// <param name="axes"></param>
-		/// <returns></returns>
-		private static UInt32 Count(this Axes axes)
-		{
-			UInt32 v = (UInt32)axes;
-			v = v - ((v >> 1) & 0x55555555); // reuse input as temporary
-			v = (v & 0x33333333) + ((v >> 2) & 0x33333333); // temp
-			UInt32 c = ((v + (v >> 4) & 0xF0F0F0F) * 0x1010101) >> 24; // count
-			return c;
 		}
 	}
 }
